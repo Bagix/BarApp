86ae5eef743f07c9403f850211c8ecddd2e8b885
Mateusz <borsuk1253@gmail.com>
2025-12-31 16:15:53 +0100

    add decoration to edit modal

--- src/components/admin/EditItemForm.vue
+++ src/components/admin/EditItemForm.vue
@@ -69,6 +69,11 @@ async function handleSubmit(): Promise<void> {
         <label for="tools">Przybory</label>
       </FloatLabel>
 
+      <FloatLabel class="input-element" variant="on">
+        <InputText id="tools" v-model="drinkToEdit.decoration" autocomplete="off" required />
+        <label for="tools">Dekoracja</label>
+      </FloatLabel>
+
       <Select
         v-model="drinkToEdit.baseAlcohol"
         :options="baseAlcohols"
